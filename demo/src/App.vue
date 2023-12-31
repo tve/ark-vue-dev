70ba71a6298d2788e92381ed7dff4d8b4f009da8
Thorsten von Eicken <tve@users.noreply.github.com>
2023-12-31 00:43:20 +0000

    feat(demo): add demo directory/app

---
+++ demo/src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { Teleport, ref } from "vue";
+import { Dialog } from "@ark-ui/vue";
+const open = ref(false);
+</script>
+
+<template>
+  <Dialog.Root>
+    <div style="display: flex; flex-direction: column">
+      <Dialog.Trigger>
+        <div
+          style="
+            background-color: #220088;
+            border-radius: 10px;
+            padding: 10px;
+            cursor: pointer;
+          "
+        >
+          Open Dialog
+        </div>
+      </Dialog.Trigger>
+      <Teleport to="body">
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content
+            style="
+              position: absolute;
+              left: 100px;
+              width: 200px;
+              background-color: #445;
+              padding: 10px;
+              border-radius: 10px;
+            "
+          >
+            <Dialog.Title>Dialog Title</Dialog.Title>
+            <Dialog.Description>Dialog Description</Dialog.Description>
+            <Dialog.CloseTrigger>Close</Dialog.CloseTrigger>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Teleport>
+    </div>
+  </Dialog.Root>
+</template>
